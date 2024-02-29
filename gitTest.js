7d20905b471668ed58494ca6d75b696fcac45d2a
SebZhi <127033724+SebZhi@users.noreply.github.com>
2024-02-29 11:26:16 +0700

    push

--- gitTest.js
+++ gitTest.js
@@ -22,8 +22,27 @@ class Calculator {
 const calculator = new Calculator();
 console.log(calculator.add(4, 3));      // Output: 8
 console.log(calculator.subtract(5, 3)); // Output: 2
-console.log(calculator.multiply(5, 3)); // Output: 15
-console.log(calculator.divide(5, 3));   // Output: 1.6666666666666667
+console.log(calculator.multiply(5, 6)); // output 30 
+console.log(calculator.divide(10, 2));  // Output: 5
+
+
+
+
+
 
 
 // hhhhhh
+
+  // exercise 1
+let celsius = 60;
+let fahrenheit = (celsius * 9/5) + 32;
+console.log(`${celsius} Celsius is equal to ${fahrenheit} Fahrenheit`);
+    
+
+
+let factorial = 1;
+let number = 4; // change this value for different results
+for(let i = 1; i <= number; i++){
+    factorial *= i;
+}
+console.log(`The factorial of ${number} is ${factorial}`);
