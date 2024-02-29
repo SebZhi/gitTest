9664230b66c5587e149f82c6854ce34c4bfca46c
SebZhi <127033724+SebZhi@users.noreply.github.com>
2024-02-29 10:38:54 +0700

    test commit

--- gitTest.js
+++ gitTest.js
@@ -20,9 +20,10 @@ class Calculator {
 }
 
 const calculator = new Calculator();
-console.log(calculator.add(5, 3));      // Output: 8
+console.log(calculator.add(4, 3));      // Output: 8
 console.log(calculator.subtract(5, 3)); // Output: 2
 console.log(calculator.multiply(5, 3)); // Output: 15
 console.log(calculator.divide(5, 3));   // Output: 1.6666666666666667
 
 
+// hhhhhh
